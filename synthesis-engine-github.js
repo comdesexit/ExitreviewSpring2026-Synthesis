ee8a2d33291c4776ca76d5d01be78f57870ef663
comdesexit <comdesexit@gmail.com>
2026-04-28 17:06:49 -0500

    Add files via upload

--- synthesis-engine-github.js
+++ synthesis-engine-github.js
@@ -1461,6 +1461,9 @@ const studentData = [
       "body.synthesis-dynamic-main .synthesis-sidebar-title .mtd-l1,body.synthesis-dynamic-main .synthesis-sidebar-title .mtd-l2{display:inline!important}" +
       "body.synthesis-dynamic-main .synthesis-sidebar-title .mtd-l1{margin-right:.25em}" +
       "body.synthesis-dynamic-main #student-grid{position:relative!important;z-index:1!important}" +
+      "}" +
+      "@media screen and (min-width:721px){" +
+      "body.synthesis-dynamic-main #nav-skeleton-behind{min-width:0!important;width:auto!important;max-width:none!important;flex:0 0 auto!important}" +
       "}";
     var styleTag = document.createElement("style");
     styleTag.id = STYLE_ID;
@@ -1589,7 +1592,7 @@ const studentData = [
       actions.appendChild(wrap);
     }
     createAction("Archive", archiveA && archiveA.getAttribute("href"), false);
-    createAction("Behind the scenes", behindA && behindA.getAttribute("href"), true);
+    createAction("Process", behindA && behindA.getAttribute("href"), true);
     menu.appendChild(actions);
 
     var title = document.createElement("p");
